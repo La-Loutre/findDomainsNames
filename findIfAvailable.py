78e761a052ac18e70c0944a0e286700692904e38
La-Loutre <ldouriez@gmail.com>
2016-04-11 23:31:06 +0200

    utf8

--- findIfAvailable.py
+++ findIfAvailable.py
@@ -2,6 +2,8 @@ import whois
 import time
 import random as rd
 import io
+from unidecode import unidecode
+
 result = open("availables.txt","w")
 names_file = io.open("generated_domains_names.txt","r",encoding='utf8')
 
@@ -10,8 +12,13 @@ for line in names_file:
     # We put random wait between whois call
     # to avoid been banned too rapidly
     # 200-300 ms
+    line = unidecode(line)
     time.sleep(rd.randrange(200,350)/1000.)
-    w = whois.whois(line.strip())
+    if '\n' in line:
+        line = line[0:len(line)-1]
+
+    w = whois.whois(line)
+
     if w["status"] == None:
         print("Found : "+line)
         result.write(line)
