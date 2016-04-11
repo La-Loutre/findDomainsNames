73f4d74ea8985fb0d78bb1af695a871705232ffd
La-Loutre <ldouriez@gmail.com>
2016-04-11 23:08:52 +0200

    utf8 correction

--- findIfAvailable.py
+++ findIfAvailable.py
@@ -1,9 +1,9 @@
 import whois
 import time
 import random as rd
-
+import io
 result = open("availables.txt","w")
-names_file = open("generated_domains_names.txt","r")
+names_file = io.open("generated_domains_names.txt","r",encoding='utf8')
 
 i = 0
 for line in names_file:
@@ -11,10 +11,10 @@ for line in names_file:
     # to avoid been banned too rapidly
     # 200-300 ms
     time.sleep(rd.randrange(200,350)/1000.)
-    print(line)
-    w = whois.whois(line)
+    w = whois.whois(line.strip())
     if w["status"] == None:
         print("Found : "+line)
         result.write(line)
         result.write('\n')
+
 result.close()
